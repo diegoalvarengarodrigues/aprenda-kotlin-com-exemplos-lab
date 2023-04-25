99a9d8862e86b40350b0556b66271b6fc67640e4
diegoalvarengarodrigues <126590830+diegoalvarengarodrigues@users.noreply.github.com>
2023-04-25 19:00:45 -0300

    solucao

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,77 @@
 // [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+enum class Nivel { BASICO, INTERMEDIARIO, AVANÇADO }
 
-class Usuario
+data class Usuario(val nome: String, val email: String)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(val nome: String, val duracao: Int, val nivel: Nivel)
 
 data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
 
     val inscritos = mutableListOf<Usuario>()
-    
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+
+    fun inscreverSe(usuario: Usuario) {
+        inscritos.add(usuario)
+    }
+    fun listaDeFormacoes(){
+        for (cont in conteudos){
+            println("${cont.nome} : ${cont.duracao}")
+        }
+    }
+    fun listaDeIncritos(): List<String> {
+        val lista = mutableListOf<String>()
+        for (user in inscritos){
+            lista.add(user.toString())
+        }
+        return lista
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val formacaoJAVA = ConteudoEducacional("Formação Java", 50, Nivel.AVANÇADO)
+    val cursoHTML = ConteudoEducacional("Curso de HTML", 20, Nivel.BASICO)
+    val cursoMySql = ConteudoEducacional("Curso de MySql", 35, Nivel.INTERMEDIARIO)
+
+    val listConteudos = mutableListOf<ConteudoEducacional>()
+    listConteudos.add(cursoHTML)
+    listConteudos.add(cursoMySql)
+    listConteudos.add(formacaoJAVA)
+
+    val Jorge = Usuario("Jorge", "jorge@hotmail.com")
+    val Marcela = Usuario("Marcela", "marcela@gmail.com")
+    val Gael = Usuario("Gael", "gael@gmail.com")
+
+    val formacaoBackEndDeveloper = Formacao("Formação BackEnd Developer", listConteudos)
+    val formacaoFrontEndDeveloper = Formacao("Formação FrontEnd Developer", listConteudos)
+    val formacaoGit = Formacao("Formação Git ", listConteudos)
+
+    formacaoBackEndDeveloper.inscreverSe(Jorge)
+    formacaoFrontEndDeveloper.inscreverSe(Marcela)
+    formacaoGit.inscreverSe(Gael)
+
+    print("Formação: ")
+    println("${formacaoBackEndDeveloper.nome}")
+    println("${formacaoFrontEndDeveloper.nome}")
+    println("${formacaoGit.nome}")
+    println()
+
+    println("Nome e Email dos Incritos :  ")
+    for (usuario in formacaoBackEndDeveloper.listaDeIncritos()) {
+        println(usuario)
+    }
+    println("\"${formacaoBackEndDeveloper.nome}\"")
+    println()
+    for (usuario in formacaoFrontEndDeveloper.listaDeIncritos()) {
+        println(usuario)
+    }
+    println("\"${formacaoFrontEndDeveloper.nome}\"")
+    println()
+    for (usuario in formacaoGit.listaDeIncritos()) {
+        println(usuario)
+    }
+    println("\"${formacaoGit.nome}\"")
+
+
+
+
 }
